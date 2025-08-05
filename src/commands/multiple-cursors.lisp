04261fe6568b792330c8b95630f8bb962edf1473
Alec Stewart <alec@datacenterhawk.com>
2025-08-05 12:15:56 -0500

    Add command to clear cursors in current buffer.

--- src/commands/multiple-cursors.lisp
+++ src/commands/multiple-cursors.lisp
@@ -5,13 +5,14 @@
   (:lock t))
 (in-package :lem-core/commands/multiple-cursors)
 
-(define-key *global-keymap* "M-C" 'add-cursors-to-next-line)
+(define-key *global-keymap* "C-M-Down" 'add-cursors-to-next-line)
+(define-Key *global-keymap* "C-M-c" 'clear-cursors)
 
 (define-command add-cursors-to-next-line () ()
   "Duplicates the cursor under the currently existing cursors."
   (let ((cursors (buffer-cursors (current-buffer))))
-    (declare (optimize (speed 3) (safety 2))
-             (type (list lem:cursor) cursors))
+    (declare (type (list lem:cursor) cursors)
+             (optimize (speed 3) (safety 2)))
     (loop :for (cursor next-cursor) :on cursors
           :do (with-point ((p cursor))
                 (declare (type lem:cursor cursor next-cursor)
@@ -25,8 +26,8 @@
                     (make-fake-cursor p))))))
 
 (defun clear-duplicate-cursors (buffer)
-  (declare (optimize (speed 3) (safety 2))
-           (type lem:buffer buffer))
+  (declare (type lem:buffer buffer)
+           (optimize (speed 3) (safety 2)))
   (loop :for (cursor next-cursor) :on (buffer-cursors buffer)
         :do (declare (type lem:cursor cursor next-cursor))
         :when (and next-cursor (same-line-p cursor next-cursor))
@@ -34,6 +35,12 @@
              (if (eq cursor (buffer-point buffer))
                  next-cursor
                  cursor))))
+
+(define-command clear-cursors () ()
+  "Clear all cursors in the current buffer."
+  (let (buffer (current-buffer))
+    (declare (type lem:buffer buffer))
+    (clear-duplicate-cursors buffer))
 
 (defun garbage-collection-cursors ()
   (clear-duplicate-cursors (current-buffer)))
