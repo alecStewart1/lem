5984337cce3754c9b2862a716fe4db8cdeefe131
Alec Stewart <alec@datacenterhawk.com>
2025-08-05 13:15:07 -0500

    Add the ability to add cursors in more than one direction.

--- src/commands/multiple-cursors.lisp
+++ src/commands/multiple-cursors.lisp
@@ -1,29 +1,68 @@
 (defpackage :lem-core/commands/multiple-cursors
   (:use :cl :lem-core)
-  (:export :add-cursors-to-next-line)
+  (:export #:add-cursors-to-next-line
+           #:add-cursors-to-previous-line
+           #:add-cursors-to-right
+           #:add-cursors-to-left
+           #:clear-cursors)
   #+sbcl
   (:lock t))
 (in-package :lem-core/commands/multiple-cursors)
 
 (define-key *global-keymap* "C-M-Down" 'add-cursors-to-next-line)
+(define-key *global-keymap* "C-M-Up" 'add-cursors-to-previous-line)
+(define-key *global-keymap* "C-M-Right" 'add-cursors-to-right)
+(define-key *global-keymap* "C-M-Left" 'add-cursors-to-left)
 (define-Key *global-keymap* "C-M-c" 'clear-cursors)
 
-(define-command add-cursors-to-next-line () ()
-  "Duplicates the cursor under the currently existing cursors."
+(defun duplicate-cursors (&key line-step char-step move-fn duplicate-p (n 1))
+  (declare (type (or null fixnum) line-step char-step)
+           (type (or null function) move-fn)
+           (type function duplicate-p)
+           (type fixnum n)
+           (optimize (speed 3) (safety 2)))
   (let ((cursors (buffer-cursors (current-buffer))))
-    (declare (type (list lem:cursor) cursors)
-             (optimize (speed 3) (safety 2)))
+    (declare (type (list lem:cursor) cursors))
     (loop :for (cursor next-cursor) :on cursors
           :do (with-point ((p cursor))
                 (declare (type lem:cursor cursor next-cursor)
                          (type lem/buffer/internal:point p))
-                (let ((line-step 1)
-                      (char-step (point-charpos p)))
-                  (declare (type fixnum line-step char-step))
-                  (when (and (line-offset p line-step char-step)
-                             (or (null next-cursor)
-                                 (not (same-line-p p next-cursor))))
-                    (make-fake-cursor p))))))
+                (let ((column (point-charpos p)))
+                  (declare (type fixnum column))
+                  (dotimes (i (or n 1))
+                    (declare (ignore i))
+                    (let ((moved (cond (move-fn (funcall move-fn p))
+                                       (line-step (line-offset p line-step (or char-step column)))
+                                       (char-step (character-offset p char-step))
+                                       (t nil))))
+                      (unless moved (return))
+                      (when (or (null next-cursor)
+                                (not (funcall duplicate-p p next-cursor)))
+                        (make-fake-cursor p))))))))
+
+(define-command add-cursors-to-next-line (n) (:universal)
+  "Duplicates the cursor under the currently existing cursors."
+  (duplicate-cursors :line-step 1
+                     :duplicate-p #'same-line-p
+                     :n n))
+
+(define-command add-cursors-to-previous-line (n) (:universal)
+  "Duplicates the cursor above the currently existing cursors."
+  (duplicate-cursors :line-step -1
+                     :duplicate-p #'same-line-p
+                     :n n))
+
+(define-command add-cursors-to-right (n) (:universal)
+  "Duplicates the cursor to the right of the currently existing cursors."
+  (duplicate-cursors :char-step 1
+                     :duplicate-p #'point=
+                     :n n))
+
+(define-command add-cursors-to-left (n) (:universal)
+  "Duplicates the cursor to the left of the currently existing cursors."
+  (duplicate-cursors :char-step -1
+                     :duplicate-p #'point=
+                     :n n))
 
 (defun clear-duplicate-cursors (buffer)
   (declare (type lem:buffer buffer)
@@ -38,9 +77,9 @@
 
 (define-command clear-cursors () ()
   "Clear all cursors in the current buffer."
-  (let (buffer (current-buffer))
+  (let ((buffer (current-buffer)))
     (declare (type lem:buffer buffer))
-    (clear-duplicate-cursors buffer))
+    (clear-duplicate-cursors buffer)))
 
 (defun garbage-collection-cursors ()
   (clear-duplicate-cursors (current-buffer)))
