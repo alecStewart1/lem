d98ae8347933a81322a656199f5e3731a5620088
Alec Stewart <alec@datacenterhawk.com>
2025-08-11 14:31:30 -0500

    Clarify docstring on type utilty.

--- src/type-utils.lisp
+++ src/type-utils.lisp
@@ -2,16 +2,17 @@
 
 
 
-(defmacro define-list-type (name element-type &key (predicate-ftype '(function (cons) (or nil t))) export)
+(defmacro define-list-type (name element-type &key (predicate-ftype '(function (list) boolean)) export)
   "Define a predicate NAME-P and a type NAME for non-empty lists of ELEMENT-TYPE.
-PREDICATE-FTYPE specifies the `ftype` declaration for the generated predicate.
-When EXPORT is true, both the predicate and the type are exported from the
-current package."
+The predicate returns NIL for non-list arguments rather than signaling a type
+error.  PREDICATE-FTYPE specifies the `ftype` declaration for the generated
+predicate.  When EXPORT is true, both the predicate and the type are exported
+from the current package."
   (declare (type symbol name element-type)
            (type list predicate-ftype)
            (type (or nil t) export))
   (let* ((pred (intern (format nil "~A-P" (symbol-name name)) (symbol-package name)))
-         (pred-doc (format nil "Return T if every element of LST is a ~A. Returns NIL otherwise." element-type))
+         (pred-doc (format nil "Return T if LST is a proper non-empty list whose elements are of type ~A. Non-list arguments return NIL." element-type))
          (type-doc (format nil "A non-empty list whose elements are of type ~A." element-type)))
     (declare (type symbol pred)
              (type string pred-doc type-doc))
@@ -21,8 +22,9 @@ current package."
          ,pred-doc
          (declare (type (or nil t list) lst)
                   (optimize (speed 3) (safety 2)))
-         (and (consp lst)
-              (listp lst)
+         ;; Non-list arguments yield NIL rather than a type error.
+         (and (listp lst)
+              (not (endp lst))
               (every #'(lambda (el)
                         (declare (type (or nil t) el)
                                  (optimize (speed 3) (safety 2)))
